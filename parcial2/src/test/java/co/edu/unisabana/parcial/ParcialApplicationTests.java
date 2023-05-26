440de3bdbfa8b63f1af834d83d099d2e96ff191f
Abril <juanabca@unisabana.edu.co>
2023-05-26 14:33:51 -0500

    dependencias

---
+++ parcial2/src/test/java/co/edu/unisabana/parcial/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
