6a7ec56c5654a55e44897e90373b57094fd54f9a
sarapiraquive <sarapirpa@unisabana.edu.co>
2023-05-27 13:37:45 -0500

    Buscar libro por titulo

--- parcial2/src/main/java/co/edu/unisabana/parcial/Controller.java
+++ parcial2/src/main/java/co/edu/unisabana/parcial/Controller.java
@@ -1,7 +1,39 @@
 package co.edu.unisabana.parcial;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/libros")
 public class Controller {
+    List<Libros> librosList;
+
+    public Controller() {
+        this.librosList = new ArrayList<>();
+        librosList.add(new Libros("Principito", "Antoine de Saint-Exupéry", "Ficcion"));
+    }
+
+        @GetMapping(path = "/libros/todos")
+        public List<Libros> obtenerLibros() {
+            return librosList;
+        }
+
+    @GetMapping(path = "/libros/buscar/{titulo}")
+    public List<Libros> obtenerLibrosPorTitulo(@RequestParam String titulo) {
+        List<Libros> busquedaTitulo = new ArrayList<>();
+        for (Libros libros : librosList) {
+            if (libros.getTitulo().equals(titulo)) {
+                busquedaTitulo.add(libros);
+            }
+        }
+        return busquedaTitulo;
+    }
+
+
+
 }
--- parcial2/src/main/java/co/edu/unisabana/parcial/Libros.java
+++ parcial2/src/main/java/co/edu/unisabana/parcial/Libros.java
@@ -1,2 +1,17 @@
-package co.edu.unisabana.parcial;public class Libros {
+package co.edu.unisabana.parcial;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libros {
+        private String titulo;
+        private String autor;
+        private String categoria;
+
 }
